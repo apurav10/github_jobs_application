badf7334a9f9071c58cdfac51940b6de4739886c
Apurav <apuravporiya10@gmail.com>
2021-06-08 13:45:43 +0530

    created a working github jobs application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import useFetchJobs from "./Components/useFetchJobs";
+import { Container } from "react-bootstrap";
+import Job from "./Components/Job";
+import JobsPagination from "./Components/JobsPagination";
+import SearchForm from "./Components/SearchForm";
 
 function App() {
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
+
+  function handleParamChange(e) {
+    const param = e.target.name;
+    const value = e.target.value;
+    setPage(1);
+    setParams((prevParams) => {
+      return { ...prevParams, [param]: value };
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="my-4">
+      <h1 className="mb-4">Github Jobs</h1>
+      <SearchForm params={params} onParamChange={handleParamChange} />
+      <JobsPagination
+        page={page}
+        setPage={setPage}
+        hasNextPage={hasNextPage} />
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try Refreshing</h1>}
+      {jobs.map((job) => {
+        return <Job key={job.id} job={job} />;
+      })}
+      <JobsPagination
+        page={page}
+        setPage={setPage}
+        hasNextPage={hasNextPage}
+      ></JobsPagination>
+    </Container>
   );
 }
 
